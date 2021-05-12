02613626fdbe7c075cfacd650228d47a82313082
abdul ghani <mohammedghani842@gmail.com>
2021-05-12 16:40:45 -0700

    added what'sapp feature

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('#myDiv').floatingWhatsApp({
+    phone: '+971 501527347',
+    popupMessage: 'Hello, how can i help you?',
+    message: "I want to sanitize my office",
+    showPopup: true,
+    showOnIE: false,
+    headerTitle: 'Welcome!',
+    headerColor: 'crimson',
+    backgroundColor: 'crimson',
+    position:"right",
+    // buttonImage: '<img src="burger.svg" />'
+});
+  });
