680dde971ece8ef70693f53fe78774e5c2db9b52
abdul ghani <mohammedghani842@gmail.com>
2021-05-31 20:10:05 -0700

    final change

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(function () {
     $('#myDiv').floatingWhatsApp({
     phone: '+971 501527347',
     popupMessage: 'Hello, how can i help you?',
-    message: "for free inspection please whats'ap",
+    message: "I need free inspection.",
     showPopup: true,
     showOnIE: false,
     headerTitle: 'Welcome!',
